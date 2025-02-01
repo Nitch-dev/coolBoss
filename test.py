3ab3b86d24fe99cfc7c9be9a437e6aeba836bfe6
ɴ ɪ ᴛ ᴄ ʜ <96729153+Nitch-dev@users.noreply.github.com>
2025-02-01 18:46:04 +0500

    final fix

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ old_listSPe = []
 change_ent = []
 change_entSPe = []
 keyCheck = ["m28","m27","m32","m51","m53","m59"]
+# keyCheck = ["m28"]
+
 
 def StringCheckinList(string, item_list):
 
@@ -18,7 +20,8 @@ def StringCheckinList(string, item_list):
 
     return False  
 def compare(old,new):
-            print(new)
+            print("New list: ",new)
+            print("Old list ",old)
             print("Comparing thje old and new data now")
             res = [x for x in new if x not in old]
             if(res): #not equal means
@@ -27,51 +30,52 @@ def compare(old,new):
             else:
                 return False
 
-def SpecialStuff(p,ke,old_listSPe,new_listSPe):
-    
-    # imp monitering special ones
-
-    print("Checking this",ke)
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto(f"https://collect.fifa.com/marketplace?tags={ke}")
-    page.wait_for_load_state('networkidle')
-
-    titles = page.locator(".browse-collectible-item_title__dgUCS").all()
-    links = page.locator(".browse-collectible-item_root__4t9EB").all()
-    price = page.locator(".browse-collectible-item_priceStartingAt__RAz61").all()
-    print("Selected")
-    i = 0
-    for t in titles:
-        tt = str(t.inner_text())
-        l = links[i].get_attribute('href')
-        p = price[i].inner_text()
-        
-        message = f"{p} , {tt} , https://collect.fifa.com{l}"
-        new_listSPe.append(message)
-        print("appending Done")
-        i = i+1
-    
-    change_entSPe = compare(old_listSPe,new_listSPe)
-    if(change_entSPe != False):
-        print("So it was False")
-        for f in change_entSPe:
-            print("Sending req to TG",f)
-            #lichi URL
-            base = f"https://api.telegram.org/bot7821071523:AAH4T1ZlXLSoltjSF0ep89r1sjB97InuUqA/sendMessage?chat_id=6780967733&text={g}"
-            print(base)
-            requests.get(base) #send msg
-            time.sleep(2) #wait so no spamming
-            
-    old_listSPe = list(new_listSPe) #add all new entries into old one
-    print("Done for m28 or whatever")
 
 while(True):
     print("After 2 Minute Running Start")
     with sync_playwright() as p:
+        
+        
         for m in range(len(keyCheck)):
-            SpecialStuff(p,keyCheck[m],old_listSPe,new_listSPe)
-            print("Done ",m)
+            # imp monitering special ones
+            print("Checking this",keyCheck[m])
+            print("2 reasons why here maybe first time and if not then its bcz new stuff eapeared")
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+            page.goto(f"https://collect.fifa.com/marketplace?tags={keyCheck[m]}")
+            page.wait_for_load_state('networkidle')
+
+            titles = page.locator(".browse-collectible-item_title__dgUCS").all()
+            links = page.locator(".browse-collectible-item_root__4t9EB").all()
+            price = page.locator(".browse-collectible-item_priceStartingAt__RAz61").all()
+            print("Selected")
+            i = 0
+            for t in titles:
+                tt = str(t.inner_text())
+                l = links[i].get_attribute('href')
+                p = price[i].inner_text()
+                
+                message = f"{p} , {tt} , https://collect.fifa.com{l}"
+                new_listSPe.append(message)
+                print("appending Done")
+                i = i+1
+            
+            change_entSPe = compare(old_listSPe,new_listSPe)
+
+            if(change_entSPe != False):
+                
+                for f in change_entSPe:
+                    print("Sending req to TG",f)
+                    #lichi URL
+                    base = f"https://api.telegram.org/bot7821071523:AAH4T1ZlXLSoltjSF0ep89r1sjB97InuUqA/sendMessage?chat_id=6780967733&text={f}"
+                    print(base)
+                    requests.get(base) #send msg
+                    time.sleep(2) #wait so no spamming
+            print("so the change was false/")       
+            old_listSPe = list(new_listSPe) #add all new entries into old one
+            print("Done for m28 or whatever")
+
+
 
         # Get Value from User for Price and Store here
         BASE_URL = "https://api.telegram.org/bot7821071523:AAH4T1ZlXLSoltjSF0ep89r1sjB97InuUqA/getUpdates"
@@ -88,8 +92,8 @@ while(True):
         priceThreshold = res.json()['result'][-1]['message']['text']
         priceThreshold =str(priceThreshold)
         print(priceThreshold)
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
+        # browser = p.chromium.launch(headless=False)
+        # page = browser.new_page()
         page.goto(f"https://collect.fifa.com/marketplace?priceHigh={priceThreshold}00&tags=right-to-buy")
 
         page.wait_for_load_state('networkidle')
@@ -127,4 +131,4 @@ while(True):
         old_list = list(new_list) #add all new entries into old one
         print("Lets sleep for 2 mint Now")
         browser.close()
-        time.sleep(120)
+        time.sleep(30)
